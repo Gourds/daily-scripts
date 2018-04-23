2d4d900c7a84e893cde213894b830568dd1483b3
arvon <yafeng2011@126.com>
2018-04-23 18:10:06 +0800

    Support e3w view  and optimize register logic

--- python-scripts/register-etcd-from-files.py
+++ python-scripts/register-etcd-from-files.py
@@ -11,6 +11,7 @@
 5. Add null file support
 6. Add null dircectory support
 7. Add etcd v3 api support
+8. Support e3w https://github.com/soyking/e3w
 '''
 
 
@@ -27,35 +28,42 @@ except IndexError as e:
     print 'Usage: %s /etcd_root to_register_dir etcd_address' % sys.argv[0]
     exit(7)
 
+
 def get_regist_file():
     try:
+        create_etcd_dir(etcd_api_version, etcddir)
         for root,dirs,file_name in os.walk(directory,topdown=False):
+            create_etcd_dir(etcd_api_version, os.path.join(etcddir, root))
+            for v_dir in dirs:
+                c_dir_name = os.path.join(etcddir,root,v_dir)
+                create_etcd_dir(etcd_api_version,c_dir_name)
             for v_file in file_name:
                 key_file = os.path.join(root,v_file)
-                #print key_file
+                c_dir_name = os.path.join(etcddir, root, v_file)
+                create_etcd_dir(etcd_api_version, c_dir_name)
                 get_file_value(key_file)
-            for v_dir in dirs:
-                key_dir = os.path.join(root,v_dir)
-                # print key_dir
-                try:
-                    requests.put('http://%s:2379/%s/keys%s/%s' % (etcdhost, etcd_api_version, etcddir, key_dir),
-                                  data={'dir': True})
-                    #print 'http://%s:2379/v2/keys%s/%s' % (etcdhost, etcddir, key_dir)
-                except requests.exceptions.ConnectionError as e:
-                    print 'Line 49 Check Address: %s' % (etcdhost)
-                    exit(7)
     except OSError as e:
-        print 'Line 52 Check the Path: %s' % (directory)
+        print 'Line %s Check the Path: %s' % (sys._getframe().f_lineno,directory)
         exit(8)
 
-def get_file_value(file_path):
-    if os.path.getsize(file_path) == 0:
+def create_etcd_dir(etcd_version,dir_name):
+    # print etcd_version,dir_name
+    if etcd_version == 'v2':
         try:
-            requests.put('http://%s:2379/%s/keys%s/%s' %(etcdhost, etcd_api_version, etcddir, file_path ), data={'dir':True})
-            # print 'http://%s:2379/v2/keys%s/%s' %(etcdhost,etcddir,file_path )
+            requests.put('http://%s:2379/%s/keys%s' % (etcdhost, etcd_api_version, dir_name),
+                              data={'dir': True})
         except requests.exceptions.ConnectionError as e:
-            print 'Line 61 Check Address: %s' % (etcdhost)
-            exit(7)
+            print 'Line %s Error Check Address: %s' % (sys._getframe().f_lineno,etcdhost)
+    elif etcd_version == 'v3':
+        regist_api_etcd(etcd_version, dir_name, 'etcdv3_dir_$2H#%gRe3*t')
+        '''
+        must use etcdv3_dir_$2H#%gRe3*t as the value can support e3w
+        '''
+    else:
+        print 'Line %s Error version option: %s' % (sys._getframe().f_lineno ,etcd_version)
+        exit(2)
+
+def get_file_value(file_path):
     with open(file_path,'r') as f1:
         for each_line in f1.readlines():
             if each_line.startswith('#'):
@@ -73,19 +81,13 @@ def get_file_value(file_path):
                     line_value = 'Null'
                 print os.path.join(etcddir,file_path,line_key),line_value
                 regist_api_etcd(etcd_api_version,os.path.join(etcddir, file_path, line_key), line_value)
-                #regist_http_etcd(etcd_api_version,os.path.join(etcddir, file_path, line_key), line_value)
-            # return (line_key,line_value)
 
 def regist_api_etcd(api_version, m_key,m_value):
     if api_version == 'v2':
-        requests.put('http://%s:2379/%s/keys%s' % (etcdhost, etcd_api_version, m_key), data={'value': m_value})
-        #try:
-        #import etcd
-        #client = etcd.client(host=etcdhost, port=2379)
-        #client.write(m_key,m_value)
-        #except AttributeError as e:
-        #    print 'Line 89 Please check the address %s' % (etcdhost)
-        #    exit(7)
+        try:
+            requests.put('http://%s:2379/%s/keys%s' % (etcdhost, etcd_api_version, m_key), data={'value': m_value})
+        except requests.exceptions.ConnectionError as e:
+            print 'Line %s Error Check Address: %s' % (sys._getframe().f_lineno, etcdhost)
     elif api_version == 'v3':
         try:
             import etcd3
